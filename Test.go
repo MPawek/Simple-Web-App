b89138dbd7576a755cbb84968da3cc1cd996b251
Montana Pawek <mpawek@gmail.com>
2025-10-06 18:06:36 -0700

    Test successful, moving working code to main program file

--- Test.go
+++ Test.go
@@ -1,7 +1,6 @@
 // The purpose of this code is to see if my understanding of Go and Fiber is correct
 // If everything is working properly, I should be able to visit http://localhost:5000 and see the message below
 
-// Program starts here in package main
 package main
 
 // Import fiber library
@@ -15,7 +14,7 @@ func main() {
 	// Define GET route for the URL, and send message when the route is accessed
 	// GET only retrieves data, likely what I need for the final version
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("It's working!")
+		return c.SendString("My name is Montana Pawek")
 	})
 
 	// The port where the application is listening
@@ -23,5 +22,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 }
