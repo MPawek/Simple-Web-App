65d931bcde5d3dc30d161fb3e8f64afdf3afc0df
Montana Pawek <mpawek@gmail.com>
2025-10-12 17:22:37 -0700

    Implemented Liatrio test action

--- SimpleWebApp.go
+++ SimpleWebApp.go
@@ -1,10 +1,17 @@
-// The main base of the code
+// The purpose of this code is to see if my understanding of Go and Fiber is correct.
+// This version is attempting to integrate the JSON file
 // If everything is working properly, I should be able to visit http://localhost:5000 and see the message below
 
+// Program starts here in package main
 package main
 
-// Import fiber library
-import "github.com/gofiber/fiber/v2"
+// Import fiber library for framework
+// Import time library for timestamp
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 // Main function
 func main() {
@@ -14,7 +21,19 @@ func main() {
 	// Define GET route for the URL, and send message when the route is accessed
 	// GET only retrieves data, likely what I need for the final version
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("My name is Montana Pawek")
+		// Get current time for timestamp
+		current_time := time.Now()
+
+		// Set response variable to hold fiber.Map data type, which allows us to use Unix timestamps alongside strings as data types (previously map[string]string was used, and was unable to do this)
+		response := (fiber.Map{
+			// Set "message" element to print string
+			"message": "My name is Montana Pawek",
+			// Set "timestamp" element to current_time converted to Unix
+			"timestamp": current_time.Unix(),
+		})
+
+		// Print the response map above as a JSON object
+		return c.JSON(response)
 	})
 
 	// The port where the application is listening
